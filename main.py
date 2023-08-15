33e7371fe202ca444f7f49c411086b0dfa7e428a
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2023-08-15 15:31:52 +0530

    chat model

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
+# from langchain.schema import SystemMessage,HumanMessage,AIMessage
 
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = OpenAI(model_name="text-davinci-003",temperature=0.5)
+llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
 
-input_data1 = input("Enter something:- \n")
-input_data2 = input("Enter lang:- \n")
+input_data = input("Enter something \n ")
 
-# prompt_template = PromptTemplate(template="What is full form of {data}",input_variables=["data"])
-# query = prompt_template.format(data=input_data)
+prompt_message = ChatPromptTemplate.from_messages(
+    [
+    SystemMessage(content="You are a helpful assistant that re-writes the user's text to sound more upbeat."),
+    HumanMessagePromptTemplate.from_template("{data}"),
+]
+)
+query_message = prompt_message.format_messages(data = input_data)
 
-# prompt_template = PromptTemplate.from_template(template="What is full form of {data}")
-# query = prompt_template.format(data=input_data)
+res = llm(query_message)
 
-prompt_template = PromptTemplate(template="Translate {sentence} to {lang}",input_variables=["sentence","lang"])
-query = prompt_template.format(sentence=input_data1,lang=input_data2)
-
-print(query)
-
-res = llm(query)
 print(res)
+
+# llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+# messages = [SystemMessage(content="You are AWS certified solutions architect, help me to learn AWS"),
+#             HumanMessage(content="Hi, what is AWS?")
+# ]
+
+# res = llm(messages)
+# print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
