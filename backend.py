eb1e6e2eb349558008f6df0af50fc5ef9dca2bd2
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2023-09-03 16:55:50 +0530

    chat_history

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
 from langchain.vectorstores import Pinecone
@@ -15,7 +15,7 @@ pinecone.init(
     environment=os.environ.get("PINECONE_REGION")
 )
 
-def rag(query):
+def rag(query, chat_history):
 
     embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
     vector_store = Pinecone.from_existing_index(index_name="chatpdf", embedding=embedding)
@@ -25,8 +25,11 @@ def rag(query):
 
     chat_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5, verbose=True)
 
-    chain = RetrievalQA.from_chain_type(llm=chat_model, chain_type="stuff", retriever=retriever, verbose = True)
+    # chain = RetrievalQA.from_chain_type(llm=chat_model, chain_type="stuff", retriever=retriever, verbose = True)
+    chain = ConversationalRetrievalChain.from_llm(llm=chat_model, retriever=retriever, verbose=True)
 
-    res = chain({"query":query})
+    # res = chain({"query":query})
+    res = chain({"question":query, "chat_history":chat_history})
     
     return res
+
--- server.py
+++ server.py
@@ -7,11 +7,32 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/chat')
+def json_to_chathistory(history):
+    res = []
+    for each in history:
+        res.append((each[0], each[1]))
+    return res
+
+@app.route("/chat", methods=["POST"])
 def chat():
-    query = request.args.get("input")
-    res = rag(query)
-    return jsonify(res)
+    body = request.get_json()
+    # print(body.get("input"), body.get("chat_history"))
+    query = body.get("input")
+    chat_history = json_to_chathistory(body.get("chat_history"))
+
+    res = rag(query, chat_history)
+
+    print(res)
+    
+    return jsonify(
+        {
+            "query": res["question"],
+            "result": res["answer"],
+            "chat_history": res["chat_history"] + [(res["question"], res["answer"])],
+        }
+    )
+
+
 
 
 if __name__ == "__main__":
