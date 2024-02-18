08c860f1b0c91aff894ac425b2f24e3c468a8c43
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2024-02-18 19:15:48 +0530

    v2

--- backend.py
+++ backend.py
@@ -1,27 +1,27 @@
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain_openai import OpenAI
 from langchain.output_parsers import CommaSeparatedListOutputParser
+from langchain_core.prompts import PromptTemplate
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
+
 def domain_name_generator(niche):
-    llm = OpenAI(model_name="text-davinci-003",temperature=1)
+    llm = OpenAI()
 
     output_parser = CommaSeparatedListOutputParser()
     format_instruction = output_parser.get_format_instructions()
 
-    prompt_template = PromptTemplate(template = """You are SEO expert having 10 years of experience. 
+    prompt_template = PromptTemplate(
+        template="""You are SEO expert having 10 years of experience. 
                                     Suggest me 3 SEO optimized .com domain names for my blog in niche {niche} \n {format_instruction}""",
-                                    input_variables=["niche"],
-                                    partial_variables={"format_instruction":format_instruction})
+        input_variables=["niche"],
+        partial_variables={"format_instruction": format_instruction},
+    )
 
     query = prompt_template.format(niche=niche)
 
     res = output_parser.parse(llm(query))
     print(res)
-
     return res
-
-
-
